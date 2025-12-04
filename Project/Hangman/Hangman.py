fbf140bdc49123d936e146780c466982ef8fc7c5
gurjikneli <48918226+gurjikneli@users.noreply.github.com>
2025-12-04 17:37:10 +0400

    Update Hangman.py

--- Project/Hangman/Hangman.py
+++ Project/Hangman/Hangman.py
@@ -3,9 +3,33 @@ import json
 import random
 from hangman_art import logo, stages
 
+
 # display encrypted word, with dashes
 def encrypt_word(word, guessed):
-    return ''.join([letter if letter in guessed else '_' for letter in word])
+    return ' '.join([letter if letter in guessed else '_' for letter in word])
+
+
+def display_lives(lives, tries, color_list):
+    print(f"LIVES REMAINING: {color_list[1]}{lives}{color_list[3]}/{color_list[0]}{tries}{color_list[3]}\n")
+
+
+def menu():
+    print(f"\n1 - GUESS THE WORD:")
+    print(f"2 - GUESS THE LETTER:\n")
+    choice = input("ENTER THE OPTION: ").lower()
+    print()
+    return choice
+
+
+def display_win(encrypted, color_list):
+        print(f"\n***** {color_list[0]}{encrypted.upper()}{color_list[3]} *****")
+        print(f"\n{color_list[0]}CONGRATULATIONS! YOU GUESSED THE WORD!{color_list[3]}\n")
+
+
+def display_loss(chosen_word, color_list):
+    print(f"\n{color_list[1]}YOU'VE LOST!{color_list[3]} THE WORD WAS: {color_list[0]}{chosen_word.upper()}{color_list[3]}")
+
+
 
 
 def game():
@@ -20,41 +44,63 @@ def game():
 
     print(logo)
     chosen_word = random.choice(list(words_dict.keys()))
-    lives = 6
+    tries = lives = 6
 
     print(stages[lives])
-    print(f"You have {lives} attempts.\n")
-    print(f"The word is {len(chosen_word)} letters long.\n")
-    print(f"The word is: {words_dict.get(chosen_word)}")
+    print(f"YOU'VE GOT {color_list[0]}{tries}{color_list[3]} ATTEMPTS TO GUESS THE WORD.\n")
+    print(f"LENGTH OF THE WORD: {color_list[2]}{len(chosen_word)}{color_list[3]}")
+    print(f"DESCRIPTION: {color_list[0]}{words_dict.get(chosen_word)}{color_list[3]}\n")
 
     guessed_letters = set()
 
-    while lives > 0: # თუ ეს ციკლი დასრულდა თავისით (return-ის) გარეშე, ეს აუცილებლად ნიშნავს, რომ მოთამაშეს ცდები ამოეწურა და წააგო.
-        print(encrypt_word(chosen_word, guessed_letters))
-        letter = input("Enter a letter: ").lower()
+    # Validation that option is '1' or '2':
+    while True:
+        options = menu()
+        if options in ['1', '2']:
+            break
 
-        # Already guessed
-        if letter in guessed_letters:
-            print("You've already guessed that letter!\n")
-            continue
+    if options == '1':
+        word = input("\nENTER THE WORD: ").lower()
+        if word == chosen_word:
+            encrypted = word
+            display_win(encrypted, color_list)
+        else:
+            display_loss(chosen_word, color_list)
 
-        guessed_letters.add(letter)
+    elif options == '2':
+        while lives > 0: # თუ ეს ციკლი დასრულდა თავისით (return-ის) გარეშე, ეს აუცილებლად ნიშნავს, რომ მოთამაშეს ცდები ამოეწურა და დამარცხდა.
+            encrypted = encrypt_word(chosen_word, guessed_letters)
+            print(encrypted, '\n')
 
-        # Wrong guess
-        if letter not in chosen_word:
-            lives -= 1
-            print(f"{color_list[1]}Wrong! Letter not in word.{color_list[3]}")
-            print(stages[lives])
-            print(f"Lives remaining: {color_list[1]}{lives}{color_list[3]}\n")
+            # If letter is in alphabet validation
+            letter = ''
+            while not letter.isalpha():
+                letter = input("\nENTER A LETTER: ").lower()
 
-        # Check win
-        encrypted = encrypt_word(chosen_word, guessed_letters)
-        if "_" not in encrypted:
-            print(f"\n****** {color_list[0]}{encrypted.upper()}{color_list[3]} *****")
-            print(f"\n{color_list[0]}CONGRATULATIONS! YOU GUESSED THE WORD!{color_list[3]}\n")
-            return
+            # Already guessed
+            if letter in guessed_letters:
+                print("YOU'VE ALREADY GUESSED THAT LETTER!\n")
+                display_lives(lives, tries, color_list)
+                continue
 
-    print(f"\n{color_list[1]}YOU LOST! The word was: {color_list[0]}{chosen_word.upper()}{color_list[3]}")  # ამიტომ ვბეჭდავთ ციკლს გარეთ ამ წინადადებას.
+            guessed_letters.add(letter)
+
+            # Wrong guess
+            if letter not in chosen_word:
+                lives -= 1
+                print(f"{color_list[1]}WRONG! THE LETTER NOT IN THE WORD.{color_list[3]}")
+                print(stages[lives])
+
+            # Check win
+            encrypted = encrypt_word(chosen_word, guessed_letters)
+            display_lives(lives, tries, color_list)
+
+            if "_" not in encrypted:
+                display_win(encrypted, color_list)
+                return
+
+        # ამიტომ პირდაპირ ვბეჭდავთ ციკლის გარეთ ამ წინადადებას.
+        display_loss(chosen_word, color_list)
 
 
 # Main loop
